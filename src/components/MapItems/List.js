637506c0483b439711a3ad1f34f649193943e02d
Steven Vandevelde <icid.asset@gmail.com>
2016-08-21 17:18:25 +0200

    Fixes #11

--- src/components/MapItems/List.js
+++ src/components/MapItems/List.js
@@ -1,6 +1,8 @@
 import { createElement } from 'react';
 import map from 'lodash/fp/map';
 import toPairs from 'lodash/fp/toPairs';
+
+import Infinite from 'react-list';
 import TrashCanIcon from 'react-icons/lib/go/trashcan';
 
 import styles from './List.pcss';
@@ -16,9 +18,36 @@ const remove = (mapItemID, removeMapItem) => () => {
 };
 
 
-const List = ({ items, removeMapItem, sortedBy }) => (
-  <div className={styles.List}>
+const renderItem = ({ items, removeMapItem, sortedBy }) => (idx, key) => {
+  const i = items[idx];
 
+  return (
+    <tr key={key}>
+      <td>
+      {
+        map(
+          x => (
+            <div className={styles.dataRow} key={x[0]}>
+              <span>{x[0]}</span>
+              <div>{x[0] === sortedBy ? <strong>{x[1]}</strong> : x[1]}</div>
+            </div>
+          ),
+          toPairs(i.attributes)
+        )
+      }
+      </td>
+      <td data-action>
+        <a onClick={remove(i.id, removeMapItem)}>
+          <TrashCanIcon />
+        </a>
+      </td>
+    </tr>
+  );
+};
+
+
+const List = (props) => (
+  <div className={styles.List}>
     <Table>
       <thead>
         <tr>
@@ -26,32 +55,12 @@ const List = ({ items, removeMapItem, sortedBy }) => (
           <th data-action>&nbsp;</th>
         </tr>
       </thead>
-      <tbody>
-        {map(i => (
-          <tr key={i.id}>
-            <td>
-            {
-              map(
-                x => (
-                  <div className={styles.dataRow} key={x[0]}>
-                    <span>{x[0]}</span>
-                    <div>{x[0] === sortedBy ? <strong>{x[1]}</strong> : x[1]}</div>
-                  </div>
-                ),
-                toPairs(i.attributes)
-              )
-            }
-            </td>
-            <td data-action>
-              <a onClick={remove(i.id, removeMapItem)}>
-                <TrashCanIcon />
-              </a>
-            </td>
-          </tr>
-        ), items)}
-      </tbody>
+      <Infinite
+        itemRenderer={renderItem(props)}
+        itemsRenderer={(items, ref) => <tbody ref={ref}>{items}</tbody>}
+        length={props.items.length}
+      />
     </Table>
-
   </div>
 );
 
