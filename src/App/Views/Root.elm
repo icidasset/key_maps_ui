216c91f60ca37e114b25ca14ccac406d5176728e
Steven Vandevelde <icid.asset@gmail.com>
2016-11-27 23:37:45 +0100

    Add auth-screen bits

--- src/App/Views/Root.elm
+++ src/App/Views/Root.elm
@@ -2,10 +2,12 @@ module Views.Root exposing (view)
 
 import Html exposing (..)
 import Model.Types exposing (Model)
+import Views.AuthScreen as AuthScreen
 
 
 view : Model -> Html msg
-view _ =
-    div
-        []
-        [ text "Hello world!" ]
+view model =
+    if model.authenticatedWith == Nothing then
+        AuthScreen.view model
+    else
+        div [] [ text "Hello world!" ]
